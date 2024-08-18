5d75d0c2b0e1b6549ec0932d9870fb57b0db0cc5
Aslan Assylkhanov <AssylkhanovAslan@gmail.com>
2024-08-18 11:00:00 +0500

    Rebased

--- app/src/main/java/kz/singularity/jetpackcomposemost/MainActivity.kt
+++ app/src/main/java/kz/singularity/jetpackcomposemost/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
@@ -16,12 +20,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
 import kz.singularity.jetpackcomposemost.ui.theme.JetpackComposeMostTheme
-import kz.singularity.jetpackcomposemost.users.UsersScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,26 +38,69 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             JetpackComposeMostTheme {
-                UsersScreen()
+                val navController = rememberNavController()
+
+                NavHost(
+                    modifier = Modifier.fillMaxSize(),
+                    navController = navController,
+                    startDestination = HelloDestination,
+                ) {
+                    composable<HelloWorldDestination> {
+                        HelloWorldScreen(navController)
+                    }
+                    composable<HelloDestination> {
+                        HelloScreen(navController)
+                    }
+//                    composable<HelloDestination> {
+//                        Box(modifier = Modifier.fillMaxSize().background(Color.Blue))
+//                    }
+                }
             }
         }
     }
 }
 
+@Serializable
+data object HelloWorldDestination
+
+@Serializable
+data object HelloDestination
+
+@Serializable
+data object RandomDestination
+
 @Composable
-fun HelloScreen() {
+fun HelloWorldScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.Red)
+            .padding(16.dp)
+            .background(Color.Blue),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello World!",
+            modifier = Modifier.clickable { navController.navigate(RandomDestination) })
+
+    }
+}
+
+@Composable
+fun HelloScreen(navController: NavController) {
     val viewModel = viewModel(modelClass = HelloViewModel::class.java)
     val state by viewModel.state
 
     HelloContent(
         state = state,
         onNameChanged = { viewModel.onNameChanged(it) },
-        onCounterButtonClicked = { viewModel.onCounterButtonClicked() },
+        onCounterButtonClicked = { navController.navigate(HelloWorldDestination) },
     )
 }
 
 @Composable
-fun HelloContent(
+private fun HelloContent(
     state: HelloState,
     onNameChanged: (String) -> Unit,
     onCounterButtonClicked: () -> Unit,
